312a78379e256315bc3e2c640d91f5447db4ebb5
Heictor <39010251+Heictor@users.noreply.github.com>
2019-02-10 20:34:04 -0200

    Update Variables.py

--- Standard-Library/Variables.py
+++ Standard-Library/Variables.py
@@ -5,24 +5,19 @@ Created on Sat Oct 13 21:22:27 2018
 @author: heict
 """
 
-
-#I'm not gonna teach you programing basic ideas cuz by now you should already know about variables
-
 #First of all, let's define a variable name, for example
 numberOfTheBeast = 666
-#So, now you have a variable and it "contains" a value that i chose (but you can chagne as well)
+#So, now you have a variable and it "contains" a value that i chose (but you can change as well)
 #Now check if it's correct with aprint function
 print(numberOfTheBeast)
 
 '''
-You must've noticed the pattern on the name of my - our - variable right away,
+You must've noticed the pattern on the name of our variable right away,
 each first letter of each word is uppercase, the reason is: There are some rules about naming variables, classes and stuff.
-BUT we're not gonna talk about it right now
+BUT that's not useful right now
 '''
- #The triple apostrophe is also a way to write comments, useful for mutiple lines
+#The triple apostrophe is also a way to write comments, useful for mutiple lines
  
 #Let me show some other applications for variables on python
 sayMyName = 'Heisenberg' #When defining the value of the variable as a string don't forget to use single quotes
 print(sayMyName)
-
-#People say around the Lands Of Mordor that variables can contain 
