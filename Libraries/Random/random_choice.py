774b32f016502ebecf9fac0fc385056e59f27dac
Heictor <39010251+Heictor@users.noreply.github.com>
2019-02-10 18:47:30 -0300

    Add files via upload

---
+++ Libraries/Random/random_choice.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 12 03:32:48 2019
+
+@author: heict
+"""
+
+
+import random
+
+#Picks a random value from a list
+animes = ['Yu-Gi-Oh', 'DragonBall', 'Naruto'] #list
+answear = random.choice(animes) #random function used
+#printing a sentence with a random result
+print('the best anime in the world is ' + answear)
